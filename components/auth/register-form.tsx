06cd8d3bebe69759220421709866bd65e5e6bb61
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-06-05 14:22:07 +0900

    register

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -114,7 +114,7 @@ export const RegisterForm = () => {
           <FormError message={error} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full">
-            Login
+            Create an account
           </Button>
         </form>
       </Form>
