785eb90900b6a2477585e46e7a71fda3e132689b
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-06-08 15:05:31 +0900

    providers

--- routes.ts
+++ routes.ts
@@ -10,7 +10,7 @@ export const publicRoutes = ["/"];
  * These routes will redirect logged in users to /settings
  * @type {string[]}
  */
-export const authRoutes = ["/auth/login", "/auth/register"];
+export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
 
 /**
  * The prefix for API authentication routes
@@ -19,9 +19,8 @@ export const authRoutes = ["/auth/login", "/auth/register"];
  */
 export const apiAuthPrefix = "/api/auth";
 
-
 /**
- * The default redirect path after logging in 
+ * The default redirect path after logging in
  * @type {string}
  */
 export const DEFAULT_LOGIN_REDIRECT = "/settings";
