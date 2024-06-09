5f2b71271f160618d769f96791cce85a9d0768f8
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-06-09 14:29:18 +0900

    verification Resend

--- actions/login.ts
+++ actions/login.ts
@@ -5,6 +5,9 @@ import { LoginSchema } from "@/schemas";
 import { AuthError } from "next-auth";
 import { signIn } from "../auth";
 import { z } from "zod";
+import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -14,6 +17,25 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   }
 
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email does not exist!" };
+  }
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return { success: "Confirmation email sent!" };
+  }
 
   try {
     await signIn("credentials", {
@@ -31,6 +53,6 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
       }
     }
 
-    throw error; 
+    throw error;
   }
 };
