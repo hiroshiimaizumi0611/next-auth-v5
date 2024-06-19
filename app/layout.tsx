d555d09d1cd9f329a1f9a25dffd8326e76ce59fb
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2024-06-19 14:15:51 +0900

    admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default async function RootLayout({
   return (
     <SessionProvider session={session}>
       <html lang="en">
+        <Toaster />
         <body className={inter.className}>{children}</body>
       </html>
     </SessionProvider>
